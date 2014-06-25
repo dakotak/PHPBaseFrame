49bede1b5a61de57b20f3d04291daf65720e4f0e
Dakota Knutson <dakotak@outlook.com>
2014-06-25 14:10:33 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * PHPBaseFrame: My Base MVC style framework for my PHP projects.
+ * 
+ * PHP Version 5
+ *
+ * @package   PHPBaseFrame
+ * @author    Dakota Knutson <dakotak@outlook.com>
+ * 
+ */
+ 
+ /***** Application Entry Point *****/
+ 
+ 
